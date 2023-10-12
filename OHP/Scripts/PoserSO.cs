87c68a6ed92fcf6b4e49489703121814acace10c
AorusGames <71634747+CometHSolis@users.noreply.github.com>
2023-10-12 11:12:18 -0400

    Delete OHP/Scripts/PoserSO.cs

--- OHP/Scripts/PoserSO.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Pose", menuName = "OpenHandPoser/Pose Object", order = 1)]
-public class PoserSO : ScriptableObject
-{
-    public Quaternion[] Bones;
-}
