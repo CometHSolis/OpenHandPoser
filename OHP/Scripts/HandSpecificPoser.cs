cca9ca81f7197ecf2203c2738487e9747a47ff8b
AorusGames <71634747+CometHSolis@users.noreply.github.com>
2023-10-12 10:49:06 -0400

    Update HandSpecificPoser.cs
    Added support for 2 hands

--- OHP/Scripts/HandSpecificPoser.cs
+++ OHP/Scripts/HandSpecificPoser.cs
@@ -5,13 +5,81 @@ using EasyButtons;
 
 public class HandSpecificPoser : MonoBehaviour
 {
-    public Transform[] Thumb;
-    public Transform[] Index;
-    public Transform[] Middle;
-    public Transform[] Ring;
-    public Transform[] Pinky;
+    public Hand leftHand;
+    public Hand rightHand;
 
     public HandSpecficiPoserSO pose;
+
+    [System.Serializable]
+    public struct Hand
+    {
+        public Transform[] Thumb;
+        public Transform[] Index;
+        public Transform[] Middle;
+        public Transform[] Ring;
+        public Transform[] Pinky;
+
+        public void SavePose(HandSpecficiPoserSO pose)
+        {
+            AssignBonePose(Thumb, pose.Thumb, true);
+            AssignBonePose(Index, pose.Index, true);
+            AssignBonePose(Middle, pose.Middle, true);
+            AssignBonePose(Ring, pose.Ring, true);
+            AssignBonePose(Pinky, pose.Pinky, true);
+        }
+
+        public void LoadPose(HandSpecficiPoserSO pose)
+        {
+            AssignBonePose(Thumb, pose.Thumb, false);
+            AssignBonePose(Index, pose.Index, false);
+            AssignBonePose(Middle, pose.Middle, false);
+            AssignBonePose(Ring, pose.Ring, false);
+            AssignBonePose(Pinky, pose.Pinky, false);
+        }
+
+        public void SetPose(Hand hand)
+        {
+            AssignBonePose(Thumb, hand.Thumb, false);
+            AssignBonePose(Index, hand.Index, false);
+            AssignBonePose(Middle, hand.Middle, false);
+            AssignBonePose(Ring, hand.Ring, false);
+            AssignBonePose(Pinky, hand.Pinky, false);
+        }
+    }
+
+#region Main Functions
+    [Button]
+    public void SavePose()
+    {
+        leftHand.SavePose(pose);
+        rightHand.SavePose(pose);
+    }
+
+    [Button]
+    public void LoadPose()
+    {
+        leftHand.LoadPose(pose);
+        rightHand.LoadPose(pose);
+    }
+
+    [Button]
+    public void MirrorLeftToRight()
+    {
+        rightHand.SetPose(leftHand);
+    }
+
+    [Button]
+    public void MirrorRightToLeft()
+    {
+        leftHand.SetPose(rightHand);
+    }
+#endregion
+
+#region Utilities
+    Quaternion MirrorBoneFix(Quaternion quat)
+    {
+        return new Quaternion(quat.x, quat.y * -1f, quat.z * -1f, quat.w);
+    }
 
     public void AssignBonePose(Transform[] bones, Quaternion[] fileBones, bool save)
     {
@@ -30,29 +98,5 @@ public class HandSpecificPoser : MonoBehaviour
             }
         }
     }
-
-    [Button]
-    public void SavePose()
-    {
-        AssignBonePose(Thumb, pose.Thumb, true);
-        AssignBonePose(Index, pose.Index, true);
-        AssignBonePose(Middle, pose.Middle, true);
-        AssignBonePose(Ring, pose.Ring, true);
-        AssignBonePose(Pinky, pose.Pinky, true);
-    }
-
-    [Button]
-    public void LoadPose()
-    {
-        AssignBonePose(Thumb, pose.Thumb, false);
-        AssignBonePose(Index, pose.Index, false);
-        AssignBonePose(Middle, pose.Middle, false);
-        AssignBonePose(Ring, pose.Ring, false);
-        AssignBonePose(Pinky, pose.Pinky, false);
-    }
-
-    Quaternion MirrorBoneFix(Quaternion quat)
-    {
-        return new Quaternion(quat.x, quat.y * -1f, quat.z * -1f, quat.w);
-    }
+#endregion
 }
