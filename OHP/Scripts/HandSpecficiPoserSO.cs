7672712eb301b69c3a26d80b1e9acf799933838a
AorusGames <71634747+CometHSolis@users.noreply.github.com>
2023-10-12 11:13:06 -0400

    Update HandSpecficiPoserSO.cs

--- OHP/Scripts/HandSpecficiPoserSO.cs
+++ OHP/Scripts/HandSpecficiPoserSO.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Pose", menuName = "OpenHandPoser/Hand Specific Pose Object", order = 1)]
+[CreateAssetMenu(fileName = "Pose", menuName = "OpenHandPoser/HandPose Object", order = 1)]
 public class HandSpecficiPoserSO : ScriptableObject
 {
     public Quaternion[] Thumb;
